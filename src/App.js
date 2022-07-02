3c9b99959fd192f1e4d8948bd2add5ce9b8e6d49
jenniferwvng <83246937+jenniferwvng@users.noreply.github.com>
2022-07-02 23:45:48 +0200

    initialize git

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+      <p>Usecontext example app</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
